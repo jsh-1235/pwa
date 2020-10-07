cd22791cbee8945120838ba63ed6db8f9173e57f
jsh <jsh-1235@daum.net>
2020-10-07 15:18:48 +0900

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
+        <h1>담배 피고 올께 ㅋㅋㅋ</h1>
         <p>PWA</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Progressive Web App
